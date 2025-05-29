f5ce078c05d3d7ffed07d902033c9f45173feceb
Liam Houlahan <liamhoulahan@gmail.com>
2025-05-29 12:51:13 +1200

    MINOR

--- spec/services/income_tax_calculator_spec.rb
+++ spec/services/income_tax_calculator_spec.rb
@@ -1,17 +1,17 @@
 require "rails_helper"
 
 describe IncomeTaxCalculator, type: :service do
-  describe "Calculates the tax for individual income at each tax bracket." do
+  describe "calculates the tax for individual income at each tax bracket" do
     {
       10_000 => 1_050.00,
       35_000 => 5_033.00,
       100_000 => 22_877.50,
       220_000 => 64_877.50
-    }.each do |annual_income, expected_tax|
+    }.each do |annual_income, expected_income_tax|
       it "calculates the correct tax for annual income of #{annual_income}" do
         individual = IndividualIncomeTax.new(annual_income: annual_income)
         result = described_class.new.calculate(individual_income_tax: individual)
-        expect(result).to eq(expected_tax)
+        expect(result).to eq(expected_income_tax)
       end
     end
   end
